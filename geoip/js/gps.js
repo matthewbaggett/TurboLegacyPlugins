06dc22a73aa84e725c8f5385e168ac091c30d657
greyscale <greyscale@2dbfba43-826e-0410-a54b-c69c240d4355>
2010-09-29 21:17:36 +0000

    git-svn-id: http://svn.turbocrms.com/projects/plugins@538 2dbfba43-826e-0410-a54b-c69c240d4355

--- geoip/js/gps.js
+++ geoip/js/gps.js
@@ -1,12 +1,20 @@
-function success(){
-	alert("gps OK");
+function success(position){
+	alert("Position found: " + position.coords.latitude + " by " + position.coords.longitude);
 }
-function error(){
-	alert("error: not-supported");
+function error(error){
+	if(typeof msg == 'string'){
+		alert("err: " + error);
+	}else{
+		alert("Error happened, non-string");
+	}
 }
+$(document).ready(function(){
+	alert("dookie");
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(success, error);
+		alert("supported");
+	} else {
+		alert('not supported');
+	}
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
-} else {
-  error('not supported');
-}
+});
