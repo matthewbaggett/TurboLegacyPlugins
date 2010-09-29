9f24cdbbf81ad40696b36cd32ef77dd77b197c21
greyscale <greyscale@2dbfba43-826e-0410-a54b-c69c240d4355>
2010-09-29 21:37:37 +0000

    git-svn-id: http://svn.turbocrms.com/projects/plugins@541 2dbfba43-826e-0410-a54b-c69c240d4355

--- geoip/js/gps.js
+++ geoip/js/gps.js
@@ -1,5 +1,5 @@
 function turboGPSsuccess(position){
-	console.info("Position found: " + position.coords.latitude + " by " + position.coords.longitude);
+	//console.info("Position found: " + position.coords.latitude + " by " + position.coords.longitude);
 	$.post(
 		root_path + "/log-position", 
 		{
@@ -8,6 +8,7 @@ function turboGPSsuccess(position){
 			requestid: request_id
 		}
 	);
+	alert("GPS success! At: " + position.coords.latitude + " by " + position.coords.longitude)
 }
 function turboGPSerror(error){
 	if(typeof msg == 'string'){
@@ -25,13 +26,13 @@ function turboGPSfireerror(error){
 				requestid: request_id
 			}
 		);
-	console.warn(error);
+	//console.warn(error);
 }
 $(document).ready(function(){
 	
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(turboGPSsuccess, turboGPSerror);
-		console.info("HTML5's geolocation supported");
+		//console.info("HTML5's geolocation supported");
 	} else {
 		turboGPSerror("HTML5 gelocation unsupported");
 	}
