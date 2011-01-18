5a03f9dfcd8d5191b50ae2fa7ad348f7c3229e93
greyscale <greyscale@2dbfba43-826e-0410-a54b-c69c240d4355>
2011-01-18 16:20:53 +0000

    git-svn-id: http://svn.turbocrms.com/projects/plugins@967 2dbfba43-826e-0410-a54b-c69c240d4355

--- cdn/manual_push_up.php
+++ cdn/manual_push_up.php
@@ -1,5 +1,6 @@
 <?php
-define("SERVER_OVERRIDE","live");
+define("SERVER_OVERRIDE",true);
+define("SERVER","live");
 require_once("../../turbo/turbo.inc");
 $cdn = new cdn();
 $cdn->action_pushup();
